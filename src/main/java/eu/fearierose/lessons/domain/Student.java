e4a864a98625d5b5a591262043fcedc9850d4236
FaerieRose <rosalynn5772-git@yahoo.com>
2017-03-21 14:14:30 +0100

    Final version for Angular2 lesson

--- src/main/java/eu/fearierose/lessons/domain/Student.java
+++ src/main/java/eu/fearierose/lessons/domain/Student.java
@@ -11,5 +11,14 @@ import javax.persistence.Entity;
  */
 @Entity
 public class Student extends Person {
+	private String studieRichting;
 
+	public String getStudieRichting() {
+		return studieRichting;
+	}
+	public void setStudieRichting(String studieRichting) {
+		this.studieRichting = studieRichting;
+	}
+	
+	
 }
